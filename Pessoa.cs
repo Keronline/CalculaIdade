e749aec2e1c0c7dd119512eb375f003532ba4fe3
Karoline Borges <tecnologia.karol@gmail.com>
2022-06-29 15:01:37 -0300

    Primeiro commit do projeto de cálculo da idade

---
+++ Pessoa.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultarIdade
+{
+    internal class Pessoa
+    {
+        public string Name { get; set; }
+        
+        public string BirthDate { get; set; }
+
+        public int Age { get; set; }
+
+        public Pessoa(string name, string birthDate)
+        {
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public void ShowAge() // 12/08/1985
+        {
+            int dayBirth = int.Parse(BirthDate.Substring(0,2));
+            int monthBirth = int.Parse(BirthDate.Substring(3,2));            
+            int yearBirth = int.Parse(BirthDate.Substring(6,4));
+
+            var today = DateTime.Now;
+
+            Age = today.Year - yearBirth;
+
+            if (monthBirth > today.Month || monthBirth == today.Month && dayBirth > today.Day)
+            {
+                Age -= 1;
+            }
+            
+            Console.Clear();
+            if(monthBirth == today.Month && dayBirth == today.Day)
+            {
+                Console.WriteLine($"Hoje é aniversário de {Name}! Parábens!");
+            }                       
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine($"{Name} possui {Age} anos de idade!");
+            Console.WriteLine("-----------------------------\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsultarIdade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string option = "S";
+            while (option.ToUpper() != "N") {
+
+                Console.WriteLine("Qual é o nome de quem você deseja saber a idade? ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Qual a data de nascimento da pessoa? No formato dd/mm/aaaa");
+                string birth = Console.ReadLine();
+
+                Pessoa p1 = new Pessoa(name, birth);
+                p1.ShowAge();
+
+                Console.WriteLine("Deseja realizar um novo cálculo novamente? \nS - SIM\nN - Não");
+                option = Console.ReadLine();
+                Console.Clear();
+            }
+
+            Console.WriteLine("Até mais :)\n");
+        }
+    }
+}
